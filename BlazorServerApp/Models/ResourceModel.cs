9505f7fdf916921f2d87f396503d95facd45bc32
Oloya Arthur Ovura <87750519+Ovura@users.noreply.github.com>
2023-01-27 09:21:42 +0100

    Add project files.

---
+++ BlazorServerApp/Models/ResourceModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorServerApp.Models
+{
+    public class ResourceModel
+    {
+        public int Id { get; set; }
+
+        public int AvailableResourceId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
